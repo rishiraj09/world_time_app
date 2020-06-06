3fe00792ea234eca54ed4243029c6e690e4aae45
rishiraj09 <rishirajsalam@gmail.com>
2020-06-06 23:47:02 +0530

    initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Map data = {};
+
+  @override
+  Widget build(BuildContext context) {
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
+    print(data);
+
+    // set background
+    String bgImage = data['isDaytime'] ? 'day.jpg' : 'night.jpg';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
+
+    Color lebels = data['isDaytime'] ? Colors.black : Colors.grey[300];
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/$bgImage'),
+          fit: BoxFit.cover,
+        )),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: SafeArea(
+              child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed: () async {
+                  dynamic result =
+                      await Navigator.pushNamed(context, '/location');
+                  setState(() {
+                    data = {
+                      'time': result['time'],
+                      'location': result['location'],
+                      'isDaytime': result['isDaytime'],
+                      'flag': result['flag'],
+                    };
+                  });
+                },
+                icon: Icon(
+                  Icons.edit_location,
+                  color: lebels,
+                ),
+                label: Text(
+                  'Edit Location',
+                  style: TextStyle(
+                    color: lebels,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0,
+                      color: lebels,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66.0,
+                  color: lebels,
+                ),
+              )
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
